99fcf40769caa4fe3eeb250b906c156fdb7f8d3b
mrDock21 <dan.mejia@alumnos.udg.mx>
2021-07-06 22:18:31 -0500

    Updated images path

--- js/components/shared/ads.js
+++ js/components/shared/ads.js
@@ -1,15 +1,15 @@
 const IMAGES = new Array(
-    "img/ad_small/_1.png", "img/ad_small/_2.png", "img/ad_small/_3.png",
-    "img/ad_small/_4.png", "img/ad_small/_5.png", "img/ad_small/_6.png",
-    "img/ad_small/_7.png", "img/ad_small/_8.png", "img/ad_small/_9.png",
-    "img/ad_small/_10.png", "img/ad_small/_11.png", "img/ad_small/_12.png",
-    "img/ad_small/_13.png", "img/ad_small/_14.png", "img/ad_small/_15.png"
+    "./img/ad_small/_1.png", "./img/ad_small/_2.png", "./img/ad_small/_3.png",
+    "./img/ad_small/_4.png", "./img/ad_small/_5.png", "./img/ad_small/_6.png",
+    "./img/ad_small/_7.png", "./img/ad_small/_8.png", "./img/ad_small/_9.png",
+    "./img/ad_small/_10.png", "./img/ad_small/_11.png", "./img/ad_small/_12.png",
+    "./img/ad_small/_13.png", "./img/ad_small/_14.png", "./img/ad_small/_15.png"
 );
 
 const AD_ROMANCE = new Array(
-    "img/ad_small/_16.png"
+    "./img/ad_small/_16.png"
 );
 
 const AD_NEWS = new Array(
-    "img/ad_small/_17.png"
+    "./img/ad_small/_17.png"
 );
