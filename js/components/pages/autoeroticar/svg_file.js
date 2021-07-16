83d5932a28cd180d117b70c961256cae72d087f3
mrDock21 <dan.mejia@alumnos.udg.mx>
2021-07-16 13:22:45 -0500

    Added dynamic SVG file loading

---
+++ js/components/pages/autoeroticar/svg_file.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** Reads SVG file and inserts it into the div */
+class SVGFile extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            svgContents: null
+        };
+
+        fetch(this.props.svgPath)
+            .then(r => r.text())
+            .then((text) => this.onSvgResponse(text))
+            .catch(console.error.bind(console));
+    }
+    render() {
+        return (
+            <div id={this.props.svgId} className={this.props.classes}>
+                {/** Data inserted at response callback */}
+            </div>
+        );
+    }
+
+    onSvgResponse(text) {
+        const state = {...this.state};
+        var dom = document.getElementById(this.props.svgId);
+
+        dom.innerHTML = state.svgContents = text;
+
+        this.setState(state);
+    }
+}
