b2cf5c6ae1d1cef0952cfa5684b56535d571ee22
mrDock21 <dan.mejia@alumnos.udg.mx>
2021-08-04 13:59:57 -0500

    Renamed autoeroticar images

--- js/min-js/pages/autoeroticar/autoeroticar_check_cars.js
+++ js/min-js/pages/autoeroticar/autoeroticar_check_cars.js
@@ -9,31 +9,31 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 var CARS_ON_SALE = [{
-    img: './../../../../img/autoeroticar/car_adv_5.png',
+    img: './../../../../img/autoeroticar/autoeroticar_car_adv_5.png',
     title: "2001 Turismo 4.0l Quad Cam V8",
     desc: "Two-door, five-speed manual, petrol engine, 26,000 miles, Rosso red, adjustable seats, alloy wheels, CD player, central locking, electric windows, cream leather, rear spoiler and ABS."
 }, {
-    img: './../../../../img/autoeroticar/car_adv_7.png',
+    img: './../../../../img/autoeroticar/autoeroticar_car_adv_7.png',
     title: "2003 Feltzer 3.8 supercharged V8",
     desc: "Air conditioning, alarm system, coloured bumpers, airbag on driver's side, electric exterior mirrors, one previous owner, CD/cassette player, new condition. Information/viewings only possible between 8.00 and 21.00 hrs."
 }, {
-    img: './../../../../img/autoeroticar/car_adv_1.png',
+    img: './../../../../img/autoeroticar/autoeroticar_car_adv_1.png',
     title: "2001 Banshee 300ci V8 Twin Turbo GT",
     desc: "Adjustable seats, air conditioning, alloy wheels, CD player, central locking, electric mirrors, electric windows, full service history, leather upholstery, immobiliser"
 }, {
-    img: './../../../../img/autoeroticar/car_adv_2.png',
+    img: './../../../../img/autoeroticar/autoeroticar_car_adv_2.png',
     title: "2006 PMP 600 6100cc V8 Semi",
     desc: "Automatic, diesel, 8,000 miles, silver metallic. Leather seats, ABS, air conditioning, alloy wheels, CD changer, cruise control, heated seats, multifunction steering wheel, navigation system, electronic parking aid."
 }, {
-    img: './../../../../img/autoeroticar/car_adv_3.png',
+    img: './../../../../img/autoeroticar/autoeroticar_car_adv_3.png',
     title: "2001 Schafter 6.5l V12 SC",
     desc: "Automatic, 37,000 miles, silver metallic. Air conditioning, ABS, alloy wheels, cruise control, full service history, leather seats."
 }, {
-    img: './../../../../img/autoeroticar/car_adv_4.png',
+    img: './../../../../img/autoeroticar/autoeroticar_car_adv_4.png',
     title: "2003 Intruder 4.3l V8 Limited",
     desc: "Nice black metallic paint, alloy wheels, additional front headlights, all electrically controlled, CD player, central locking, all in exceptionally good condition."
 }, {
-    img: './../../../../img/autoeroticar/car_adv_6.png',
+    img: './../../../../img/autoeroticar/autoeroticar_car_adv_6.png',
     title: "2004 Presidente 4.5l V10",
     desc: "Four-door, manual transmission, 26,000 miles, metallic turquoise, twelve-month parts and service warranty, insurance. Financing available at zero per cent PAYMENT. Ask Derek for a bespoke finance proposal. Approved finance provider. Detailed brochure available"
 }];
